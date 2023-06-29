18c222160521346f0ca51ec795dc2c1d2ba37af4
Nikola Vujovic <nikola.vujovic1988@yahoo.com>
2023-06-29 21:21:44 +0200

    shown clouds randomly on canvas

---
+++ models/cloud.class.js
@@ -0,0 +1,15 @@
+class Cloud extends MovebleObjects {
+
+    y = 50;
+    width = 500;
+    height = 250;
+
+    constructor(){
+        super().loadImage('../img/5_background/layers/4_clouds/1.png');
+        
+        this.x = Math.random() * 500;
+    
+    }
+
+    // img/5_background/layers/4_clouds/1.png
+}
--- models/world.class.js
+++ models/world.class.js
@@ -5,6 +5,9 @@ class World {
         new Chicken(),
         new Chicken(),
     ];
+    clouds = [
+        new Cloud(),
+    ]
     canvas;
     ctx;
 
@@ -23,6 +26,9 @@ class World {
             this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
         });
 
+        this.clouds.forEach(cloud => {
+            this.ctx.drawImage(cloud.img, cloud.x, cloud.y, cloud.width, cloud.height);
+        });
 
         let self = this;
         requestAnimationFrame(function() {
