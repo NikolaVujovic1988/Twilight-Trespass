e3fdb00394b93e654ceb1e795c30ecc797a03dd9
Nikola Vujovic <nikola.vujovic1988@yahoo.com>
2023-06-29 20:49:41 +0200

    shown chicken and character without bug

--- js/game.js
+++ js/game.js
@@ -3,6 +3,8 @@ let world;
 
 function init() {
     canvas = document.getElementById('canvas');
+    canvas.width = 720; 
+    canvas.height = 480;
     world = new World(canvas);
 
     console.log('My character is', world.character);
