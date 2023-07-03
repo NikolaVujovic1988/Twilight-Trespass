c1b7de56826f439757cc92699324d267e70d912a
Nikola Vujovic <nikola.vujovic1988@yahoo.com>
2023-07-03 10:32:00 +0200

    added keyboard in world and in moving right of character

--- models/character.class.js
+++ models/character.class.js
@@ -10,9 +10,9 @@ class Character extends MovebleObjects {
         '../img/2_character_pepe/2_walk/W-25.png',
         '../img/2_character_pepe/2_walk/W-26.png'
     ];
-    
+    world;
 
-    constructor(){
+    constructor() {
         super().loadImage('../img/2_character_pepe/2_walk/W-21.png');
         this.loadImages(this.IMAGES_WALKING);
         this.animate();
@@ -20,14 +20,16 @@ class Character extends MovebleObjects {
 
     animate() {
         setInterval(() => {
-            let i = this.currentImage % this.IMAGES_WALKING.length;
-            let path = this.IMAGES_WALKING[i];
-            this.img = this.imageCache[path];
-            this.currentImage++;
+            if (this.world.keyboard.RIGHT) {
+                let i = this.currentImage % this.IMAGES_WALKING.length;
+                let path = this.IMAGES_WALKING[i];
+                this.img = this.imageCache[path];
+                this.currentImage++;
+            }
         }, 75);
     }
 
-    jump(){
+    jump() {
 
     }
 }
