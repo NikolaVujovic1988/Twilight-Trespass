7f4f7fe0172e50012adf4e234dcef4415a692c83
Nikola Vujovic <nikola.vujovic1988@yahoo.com>
2023-07-05 10:52:57 +0200

    endboss added

--- models/moveble-objects.class.js
+++ models/moveble-objects.class.js
@@ -20,14 +20,21 @@ class MovebleObjects {
             let img = new Image();
             img.src = path;
             this.imageCache[path] = img;
-        });    
+        });
+    }
+
+    playAnimation(images) {
+        let i = this.currentImage % this.IMAGES_WALKING.length;
+        let path = images[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
     }
 
     moveRight() {
-        
+
     }
 
-    moveLeft(){
+    moveLeft() {
         setInterval(() => {
             this.x -= this.speed;
         }, 1000 / 60);
