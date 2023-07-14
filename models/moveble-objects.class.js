42f931f54875867d9daeeb3f3cf4a9fe6c0801fa
Nikola Vujovic <nikola.vujovic1988@yahoo.com>
2023-07-14 23:46:45 +0200

    added new classes and structure - drawable objects and statusbar

--- models/moveble-objects.class.js
+++ models/moveble-objects.class.js
@@ -1,11 +1,4 @@
-class MovebleObjects {
-    x = 120;
-    y = 280;
-    img;
-    height = 150;
-    width = 100;
-    imageCache = {};
-    currentImage = 0;
+class MovebleObjects extends DrawableObject {
     speed = 0.15;
     otherDirection = false;
     speedY = 0;
@@ -24,33 +17,6 @@ class MovebleObjects {
 
     isAboveGround() {
         return this.y < 150;
-    }
-
-    draw(ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-    }
-
-    drawFrame(ctx) {
-        if (this instanceof Character || this instanceof Chicken) {
-            ctx.beginPath();
-            ctx.lineWidth = '5';
-            ctx.strokeStyle = 'blue';
-            ctx.rect(this.x, this.y, this.width, this.height);
-            ctx.stroke();
-        }
-    }
-
-    loadImage(path) {
-        this.img = new Image();
-        this.img.src = path;
-    }
-
-    loadImages(arr) {
-        arr.forEach(path => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
     }
 
     playAnimation(images) {
